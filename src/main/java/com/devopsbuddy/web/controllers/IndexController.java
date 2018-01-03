770f83216ef8aced9fbb31d4fdb4306349b84af3
sangmessi <messi1913@gmail.com>
2018-01-03 23:08:46 +0900

    Added placeholder

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloWorldController {
-	@RequestMapping("/")
-	public String sayHello() {
-		return "index";
-	}
+public class IndexController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "index";
+    }
 }
