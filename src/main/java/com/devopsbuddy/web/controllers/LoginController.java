136eca202bc07a690ca156b58d63c2c62b4b974e
sangmessi <messi1913@gmail.com>
2018-01-14 22:53:02 +0900

    Added Login from and Login link to navbar

---
+++ src/main/java/com/devopsbuddy/web/controllers/LoginController.java
@@ -0,0 +1,27 @@
+package com.devopsbuddy.web.controllers;
+
+import com.devopsbuddy.backend.service.EmailService;
+import com.devopsbuddy.web.domain.frontend.FeedbackPojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+
+    /** The Login view name.  */
+    public static final String LOGIN_VIEW_NAME = "user/login";
+
+    @RequestMapping("/login")
+    public String login(){
+        return LOGIN_VIEW_NAME;
+    }
+}
