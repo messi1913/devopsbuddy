88ff3501f69fd1864b7784003990ac0f19eacf27
sangmessi <messi1913@gmail.com>
2018-01-21 16:43:41 +0900

    Externalised webmaster login and email details

--- src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/DevopsbuddyApplication.java
@@ -10,6 +10,7 @@ import com.devopsbuddy.utils.UserUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,15 +30,25 @@ public class DevopsbuddyApplication implements CommandLineRunner {
     @Autowired
     private UserService userService;
 
+    @Value("${webmaster.username}")
+    private String webmasterUsername;
+
+    @Value("${webmaster.password}")
+    private String webmasterPassword;
+
+    @Value("${webmaster.email}")
+    private String webmasterEmail;
+
     public static void main(String[] args) {
         SpringApplication.run(DevopsbuddyApplication.class, args);
     }
 
     @Override
     public void run(String... strings) throws Exception {
-        User user = UserUtils.createBasicUser("proUser", "proUser@sangmin.com");
+        User user = UserUtils.createBasicUser(webmasterUsername, webmasterEmail);
+        user.setPassword(webmasterPassword);
         Set<UserRole> userRoles = new HashSet<>();
-        userRoles.add(new UserRole(user, new Role(RolesEnum.BASIC)));
+        userRoles.add(new UserRole(user, new Role(RolesEnum.ADMIN)));
         LOG.debug("Creating user with name {}", user.getUsername());
         userService.createUser(user, PlansEnum.BASIC, userRoles);
         LOG.info("User {} created : ", user.getUsername());
