da9ce476fafe61e28669451fe0f243d73c234f6b
sangmessi <messi1913@gmail.com>
2018-01-25 22:43:08 +0900

    Added file upload

--- src/main/java/com/devopsbuddy/config/ProductionConfig.java
+++ src/main/java/com/devopsbuddy/config/ProductionConfig.java
@@ -2,6 +2,8 @@ package com.devopsbuddy.config;
 
 import com.devopsbuddy.backend.service.EmailService;
 import com.devopsbuddy.backend.service.SmtpEmailService;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,5 +17,12 @@ public class ProductionConfig {
     @Bean
     public EmailService emailService() {
         return new SmtpEmailService();
+    }
+
+    @Bean
+    public ServletRegistrationBean h2ConsoleServletRegistration() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
     }
 }
